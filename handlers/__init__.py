a1ab3cf6e5f38030df218d3d35a7e0842f10b01b
Aleksey Vladimirovich <33750648+sithortodox@users.noreply.github.com>
2025-05-18 20:06:33 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,9 @@
-# Package marker for routers
+from .user.start import router as start_router
+from .user.balance import router as balance_router
+from .admin.panel import router as admin_panel_router
+
+__all__ = [
+    "start_router",
+    "balance_router",
+    "admin_panel_router",
+]
