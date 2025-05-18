0b2a130283c326812fca075ebe6f62088938ca4b
Aleksey Vladimirovich <33750648+sithortodox@users.noreply.github.com>
2025-05-18 21:02:23 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,7 @@
+# handlers/__init__.py
 from .user.start import router as start_router
 from .user.balance import router as balance_router
-from .user.cabinet import router as cabinet_router  # ← новое
+from .user.cabinet import router as cabinet_router   # ← новое
 
 __all__ = [
     "start_router",
